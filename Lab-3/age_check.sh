f1cde76b8c9f001dafc2244434e376f8513c05af
devashish94 <roy.devashish94@gmail.com>
2023-01-25 16:12:20 +0530

    lab 3 completed

---
+++ Lab-3/age_check.sh
@@ -0,0 +1,7 @@
+# check if a person is eligible for voting
+read a
+if (( $a > 18 )); then 
+    echo "can vote"
+else 
+    echo "cannot vote"
+fi
--- Lab-3/conditionals.sh
+++ Lab-3/conditionals.sh
@@ -1,9 +1,8 @@
-# Simple if else
-#read a
-#read b
-#if [ $a == $b ]; then
-#	echo "$a and $b are equal!!"
-#else 
-#	echo "not equal!!"
-#fi
-
+# check if two variables are having equal value
+read a
+read b
+if [ $a == $b ]; then
+	echo "$a and $b are equal!!"
+else 
+	echo "not equal!!"
+fi
