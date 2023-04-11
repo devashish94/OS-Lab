7b28359bf3b2c4eeeb354f7eceab8af6095b3a47
devashish94 <roy.devashish94@gmail.com>
2023-04-12 03:03:35 +0530

    stuck on infinite loop

--- Lab-10/shortest_remaining_job_first.c
+++ Lab-10/shortest_remaining_job_first.c
@@ -37,22 +37,43 @@ int find_min_time(Process process_list[], size_t length) {
     return min;
 }
 
+Process shortest_job(Process process_list[], size_t length) {
+    Process min = process_list[0];
+    for (int i = 0; i < length; i++) {
+        if (process_list[i].burst_time < min.burst_time) {
+            min = process_list[i];
+        }
+    }
+    return min;
+}
+
 void shortest_remaining_time(Process process_list[], size_t length) {
     fill_temp_burst(process_list, length); // fill the temp burst time variable with the burst time
 
     int jobs = 0;
-    int time_elapsed = find_min_time(process_list, length);
+    int time = 0;
 
-    while (jobs != )
+    while (jobs != length) {
+        Process shortest_process = shortest_job(process_list, length);
+        if (shortest_process.arrival_time <= time) {
+            shortest_process.burst_time--;
+            if (shortest_process.burst_time == 0) {
+                shortest_process.burst_time = 69; // FIGURE OUT THE INFITE LOOP
+                jobs++;
+                printf("jobs: %d\n", jobs);
+            }
+        }
+        // printList(process_list, length);
+        time++;
+    }
 }
 
 int main() {
     size_t length = 3;
     Process process_list[] = {
-        {.pid = 1, .arrival_time = 0, .burst_time = 7},
-        {.pid = 2, .arrival_time = 1, .burst_time = 3},
-        {.pid = 3, .arrival_time = 3, .burst_time = 4}
+        {.pid = 1, .arrival_time = 0, .burst_time = 0},
+        {.pid = 2, .arrival_time = 1, .burst_time = 0},
+        {.pid = 3, .arrival_time = 3, .burst_time = 0}
     }; 
     shortest_remaining_time(process_list, length);
-    printList(process_list, length);
 }
