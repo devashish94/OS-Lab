17df9d1e5d9aaa1f5bbc585f4cfd4bddf539b00d
devashish94 <roy.devashish94@gmail.com>
2023-04-12 00:34:57 +0530

    started SRJF algo

--- Lab-10/shortest_remaining_job_first.c
+++ Lab-10/shortest_remaining_job_first.c
@@ -1,25 +1,58 @@
 #include <stdio.h>
 
-shortest_remaining_job(int*, int*, size_t);
+typedef struct {
+    int pid;
+    int arrival_time;
+    int burst_time;
+    int temp_burst;
+    int completion_time;
+    int turn_around_time;
+    int waiting_time;
+} Process;
 
-int main() {
-    int pid[] = {1, 2, 3};
-    int arrival_time[] = {0, 1, 2};
-    int burst_time[] = {4, 2, 1};
-    size_t length = *(&burst_time + 1) - burst_time;
-    printf("Number of Processes is: %ld\n", length);
-    shortest_remaining_job(arrival_time, burst_time);
+void printList(Process process_list[], size_t length) {
+    for(int i = 0; i < length; i++) {
+        printf(
+            "PID:\t%d\tAT:\t%d\tBT:\t%d\n", 
+            process_list[i].pid, 
+            process_list[i].arrival_time, 
+            process_list[i].burst_time
+        );       
+    }
 }
 
-shortest_remaining_job(int arrival_time[], int burst_time[], size_t length) {
-    int time = 0;
-    int jobsDone = 0;
-    while (time++ <= length) {
-        int minBurstTimeIndex = findMinBurstTimeIndex(burst_time, length);
-        burst_time[minBurstTimeIndex]--;
-        if (burst_time[minBurstTimeIndex] <= 0) {
-            jobsDone++;
-        }
-        
+void fill_temp_burst(Process process_list[], size_t length) {
+    for(int i = 0; i < length; i++) {
+        process_list[i].temp_burst = process_list[i].burst_time;
     }
+}
+
+int find_min_time(Process process_list[], size_t length) {
+    int min = process_list[0].arrival_time;
+    for (int i = 0; i < length; i++) {
+        if (process_list[i].arrival_time < min) {
+            min = process_list[i].arrival_time;
+        }
+    }
+    return min;
+}
+
+void shortest_remaining_time(Process process_list[], size_t length) {
+    fill_temp_burst(process_list, length); // fill the temp burst time variable with the burst time
+
+    int jobs = 0;
+    int time_elapsed = find_min_time(process_list, length);
+
+    while (jobs != )
+}
+
+int main() {
+    size_t length = 3;
+    Process process_list[] = {
+        {.pid = 1, .arrival_time = 0, .burst_time = 7},
+        {.pid = 2, .arrival_time = 1, .burst_time = 3},
+        {.pid = 3, .arrival_time = 3, .burst_time = 4}
+    }; 
+    shortest_remaining_time(process_list, length);
+    printList(process_list, length);
 }
